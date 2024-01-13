7ab2ea850e4caae577dfdf99bab6e6e423f0c8d6
asapoka <asapoka@outlook.com>
2024-01-13 23:33:05 +0900

    remove mklink

--- bat/mklink.bat
+++
@@ -1,4 +0,0 @@
-echo "please run this bat adminstrator"
-pause
-mklink %USERPROFILE%\.bash_profile %USERPROFILE%\dotfiles\.bash_profile
-mklink %USERPROFILE%\.bashrc	   %USERPROFILE%\dotfiles\.bashrc
