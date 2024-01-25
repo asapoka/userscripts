84b7cc160d137af28450e1ef8f22481ff24c8ab9
asapoka <asapoka@outlook.com>
2024-01-25 22:05:53 +0900

    コメントしっかり書いた

--- Violentmonkey/restaurant_check-tokyodisneyresort.jp.user.js
+++ Violentmonkey/restaurant_check-tokyodisneyresort.jp.user.js
@@ -8,10 +8,12 @@
 // @description 2023/3/29 0:11:22
 // ==/UserScript==
 
+// LINE通知メッセージ
 const params = new URLSearchParams({
   message: "レストランの空きを掴みました！",
 });
 
+// LINE通知送信
 function lineNotification() {
   console.log("notification!");
   GM.xmlHttpRequest({
@@ -27,24 +29,41 @@ function lineNotification() {
     },
   });
 }
-const f1 = async function () {
+
+// 空き状況確認
+const checkState = async function () {
+  // 空席をクリックしたらtrueにするフラグ
   clickFlg = false;
+
+  // 予約希望する時間帯
   times = ["18:00", "18:10", "18:20", "18:30", "18:40", "18:50", "19:00", "19:10", "19:20", "19:30", "19:40", "19:50"];
+
+  // 各時間帯を展開する
   $(".icon-arrow").click();
+
+  // tr要素を探索してチェック
   $("tr")
     .each(function (index, element) {
+      // 空き状況 (空席あり or 満席)
       state = $(element).children("td.state").text().trim();
+      // 予約時間
       time = $(element).children("th").text().trim();
       console.log(time + " " + state);
+      // 空席あり かつ 希望時間ならクリック
       if (state == "空席あり" && times.includes(time)) {
+        // クリックできたのでフラグオン（リロードしない）
         clickFlg = true;
+        // LINE通知送信
         lineNotification();
+        // 予約するをクリック
         $(element).children("td.btn").children("a").click();
+        // ループ中断
         return false;
       }
     })
     .promise()
     .done(function () {
+      // クリックできてければリロード
       if (clickFlg == false) {
         console.log("全部満席");
         wait_reload(1 * 1000);
@@ -52,6 +71,7 @@ const f1 = async function () {
     });
 };
 
+// 指定した時間待ってリロードする関数
 function wait_reload(t) {
   console.log("ready reload...");
   setTimeout(function () {
@@ -60,19 +80,21 @@ function wait_reload(t) {
   }, t);
 }
 
+// 要素の読み込み待ちする関数
 const wait_loading = async function () {
   t = setInterval(function () {
     if ($(".ui-mobile" + ".ui-loading").length == 0) {
       console.log("ロード完了");
+      // 監視中断
       clearInterval(t);
-      f1();
+      // 空き状況チェック関数呼び出し
+      checkState();
     }
   }, 10);
 };
 
 async function exec_workflow() {
-  // やりたいことの流れはここに記述する。
-
+  // 要素読み込み完了するまで待つ
   await wait_loading();
 }
 //////////////////////////////////////////////////////////////////////////////////////////////
