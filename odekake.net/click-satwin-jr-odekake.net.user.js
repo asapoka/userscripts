e532b767d36e54ad1a98bf3793c8181917e9c725
asapoka <asapoka@outlook.com>
2024-03-23 18:17:05 +0900

    ver up

--- odekake.net/click-satwin-jr-odekake.net.user.js
+++ odekake.net/click-satwin-jr-odekake.net.user.js
@@ -4,7 +4,7 @@
 // @match       https://e5489.jr-odekake.net/e5489/cspc/CBChangeTrainPC*
 // @match       https://e5489.jr-odekake.net/e5489/cssp/CBChangeTrainSP
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      -
 // @description 列車の変更を押下後サンライズツイン→シングルの優先度で選択をクリックします
 // ==/UserScript==
