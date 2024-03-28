2de3ddd133c74e0522a198628c2d6970d19de018
asapoka <asapoka@outlook.com>
2024-03-28 20:55:03 +0900

    予約処理完了時にLINE通知送信

--- odekake.net/final-conf-jr-odekake.net.user.js
+++ odekake.net/final-conf-jr-odekake.net.user.js
@@ -2,11 +2,33 @@
 // @name        final-conf-jr-odekake.net
 // @namespace   Violentmonkey Scripts
 // @match       https://e5489.jr-odekake.net/e5489/cspc/CBRsvFinalConfNumberSeatPC*
-// @grant       none
+// @grant       GM.xmlHttpRequest
 // @version     1.0
 // @author      -
 // @description 最終確認
 // ==/UserScript==
+
+// LINE通知メッセージ
+const params = new URLSearchParams({
+  message: "予約処理完了",
+});
+
+// LINE通知送信
+function lineNotification() {
+  console.log("notification!");
+  GM.xmlHttpRequest({
+    method: "POST",
+    url: "https://notify-api.line.me/api/notify",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer ${YOUR TOKEN}",
+    },
+    data: params.toString(),
+    onload: function (response) {
+      console.log(response.responseText);
+    },
+  });
+}
 const f1 = async function () {
   // 駅の券売機 窓口
   $(".tab-parts__trigger")[1].click();
