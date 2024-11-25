19a75332d02e2ca7aa04801a12300baada2622cf
asapoka <asapoka@outlook.com>
2024-11-25 23:03:47 +0900

    LINE通知メッセージ改善

--- tokyodisneyresort.jp/restaurant_check-tokyodisneyresort.jp.user.js
+++ tokyodisneyresort.jp/restaurant_check-tokyodisneyresort.jp.user.js
@@ -7,14 +7,19 @@
 // @author      asapoka
 // @description 2023/3/29 0:11:22
 // ==/UserScript==
-// https://reserve.tokyodisneyresort.jp/sp/hotel/list/?showWay=&roomsNum=1&adultNum=2&childNum=0&stayingDays=1&useDate=20249999&cpListStr=&childAgeBedInform=&searchHotelCD=DHM&searchHotelDiv=&hotelName=&searchHotelName=&searchLayer=&searchRoomName=%E3%82%B9%E3%83%9A%E3%83%81%E3%82%A2%E3%83%BC%E3%83%AC%E3%83%BB%E3%83%AB%E3%83%BC%E3%83%A0%EF%BC%86%E3%82%B9%E3%82%A4%E3%83%BC%E3%83%88%E3%80%80%E3%83%9D%E3%83%AB%E3%83%88%E3%83%BB%E3%83%91%E3%83%A9%E3%83%87%E3%82%A3%E3%83%BC%E3%82%BE%E3%83%BB%E3%82%B5%E3%82%A4%E3%83%89%20%E3%83%86%E3%83%A9%E3%82%B9%E3%83%AB%E3%83%BC%E3%83%A0%20%E3%83%8F%E3%83%BC%E3%83%90%E3%83%BC%E3%82%B0%E3%83%A9%E3%83%B3%E3%83%89%E3%83%93%E3%83%A5%E3%83%BC&hotelSearchDetail=true&detailOpenFlg=0&checkPointStr=&hotelChangeFlg=false&removeSessionFlg=true&returnFlg=false&hotelShowFlg=&displayType=data-hotel&reservationStatus=1
-// LINE通知メッセージ
-const params = new URLSearchParams({
-  message: "レストランの空きを掴みました！",
-});
+
+// レストラン名
+var res_name = $(".name").text();
+
+// 利用日をURLより取得
+var useDate = location.href.match(/\d{8}/);
 
 // LINE通知送信
-function lineNotification() {
+function lineNotification(msg) {
+  // LINE通知メッセージ
+  let params = new URLSearchParams({
+    message: msg,
+  });
   console.log("notification!");
   GM.xmlHttpRequest({
     method: "POST",
@@ -36,7 +41,7 @@ const checkState = async function () {
   clickFlg = false;
 
   // 予約希望する時間帯
-  times = ["19:10", "19:20", "19:30", "19:40", "19:50", "20:00", "20:10", "20:20"];
+  times = ["19:40", "19:50", "20:00"];
 
   // 各時間帯を展開する
   $(".icon-arrow").click();
@@ -50,11 +55,11 @@ const checkState = async function () {
       time = $(element).children("th").text().trim();
       console.log(time + " " + state);
       // 空席あり かつ 希望時間ならクリック
-      if (state == "空席あり" && times.includes(time)) {
+      if (state == "空席あり") {
         // クリックできたのでフラグオン（リロードしない）
         clickFlg = true;
         // LINE通知送信
-        lineNotification();
+        lineNotification(time + ":" + res_name);
         // 予約するをクリック
         $(element).children("td.btn").children("a").click();
         setTimeout(function () {
@@ -90,6 +95,7 @@ function wait_reload(t) {
 // 要素の読み込み待ちする関数
 const wait_loading = async function () {
   t = setInterval(function () {
+    document.title = useDate + ":読込中...";
     if ($(".ui-mobile" + ".ui-loading").length == 0) {
       console.log("ロード完了");
       // 監視中断
